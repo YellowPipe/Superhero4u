88236afbe943e46d1997b1a36fac2308f2f74672
YellowPipe <philippdmitrienko@gmail.com>
2019-11-19 15:57:23 +0200

    refactoring cardrow

--- src/components/CardRow/CardRow.component.js
+++ src/components/CardRow/CardRow.component.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import { API_URL } from '../../constants/constants';
+
 import './CardRow.styles.css';
 
 import Row from 'react-bootstrap/Row';
 import MyCard from '../MyCard/MyCard.component';
 
-const CardRow = ({category, heroes, history, match}) => (
-	<div>
-		<h1 onClick={ () => history.push(`${match.url}/${category}`) } className="pl-3 mt-3 title">{category}</h1>
-		<Row>
-			{
-				heroes.map((hero) => (
-					<MyCard key={hero._id} id={hero._id}  name={hero.name} salary={hero.salary} imgUrl={hero.image}
-					powerstats={{
-				        "intelligence": hero.intelligence,
-				        "strength": hero.strength,
-				        "speed": hero.speed,
-				        "durability": hero.durability,
-				        "power": hero.power,
-				        "combat": hero.combat
-				   	}}/>
-				))
-			}
-		</Row>
-	</div>
-);
+class CardRow extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			heroes: [],
+			loading: false,
+			error: null
+		};
+	};
+
+	componentDidMount() {
+		this.setState({loading: true});
+
+		fetch(`${API_URL}/heroes/${this.props.category}/3`, { method: 'get' })
+		.then(res => res.json())
+		.then(heroes => this.setState({heroes: heroes, loading: false}))
+		.catch(err => this.setState({loading: false, error: err}))
+	};
+
+	render () {
+		return (
+			<div>
+				<h1 onClick={ () => this.props.history.push(`${this.props.match.url}/${this.props.category}`) } className="pl-3 mt-3 title">{this.props.category}</h1>
+				<Row>
+					{
+						this.state.heroes.map((hero) => (
+							<MyCard key={hero._id} id={hero._id}  name={hero.name} salary={hero.salary} imgUrl={hero.image}
+							powerstats={{
+						        "intelligence": hero.intelligence,
+						        "strength": hero.strength,
+						        "speed": hero.speed,
+						        "durability": hero.durability,
+						        "power": hero.power,
+						        "combat": hero.combat
+						   	}}/>
+						))
+					}
+				</Row>
+			</div>
+		);
+	};
+};
 
 export default withRouter(CardRow);
--- src/components/ShopSection/ShopSection.component.js
+++ src/components/ShopSection/ShopSection.component.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { setHeroes } from '../../redux/heroes/heroes.actions';
-import CardRow from '../CardRow/CardRow.component';
+
+import Row from 'react-bootstrap/Row';
+import MyCard from '../MyCard/MyCard.component';
 
 class ShopSection extends React.Component {
 
@@ -13,7 +15,24 @@ class ShopSection extends React.Component {
 	render() {
 		const { heroes, category } = this.props;
 		return (
-			<CardRow category={category} heroes={heroes} />
+				<div>
+					<h1 className="pl-3 mt-3">{category}</h1>
+					<Row>
+						{
+							heroes.map((hero) => (
+								<MyCard key={hero._id} id={hero._id}  name={hero.name} salary={hero.salary} imgUrl={hero.image}
+								powerstats={{
+							        "intelligence": hero.intelligence,
+							        "strength": hero.strength,
+							        "speed": hero.speed,
+							        "durability": hero.durability,
+							        "power": hero.power,
+							        "combat": hero.combat
+							   	}}/>
+							))
+						}
+					</Row>
+				</div>
 		);
 	};
 };
